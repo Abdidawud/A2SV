090f8e3000b887dacb0556e83e9850f376afa461
Abdi Dawud <139214089+Abdidawud@users.noreply.github.com>
2023-12-24 00:58:39 +0300

    Added solution for Merge Strings Alternately

---
+++ Leetcode/merge-strings-alternately.py
@@ -0,0 +1,16 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        l1=list(word1)
+        l2=list(word2)
+        l=[]
+        x=min(len(l1),len(l2))
+        for i in range(x):
+            l.append(l1[i])
+            l.append(l2[i])
+        if i<len(l1)-1:
+            l.extend(l1[i+1:])
+        elif i<len(l2)-1:
+            l.extend(l2[i+1:])
+        return ''.join(l)
+                
+
