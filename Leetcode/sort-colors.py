197540b63547dcae4840bef1d83ff0ee12beaab4
Abdi Dawud <139214089+Abdidawud@users.noreply.github.com>
2024-01-23 20:04:17 +0300

    Added solution for Sort Colors

--- Leetcode/sort-colors.py
+++ Leetcode/sort-colors.py
@@ -3,4 +3,20 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        nums.sort()
+        L = 0
+        m = 0
+        R = len(nums)-1
+        while m<len(nums):
+            if m>R:
+                break
+            if nums[m]==0:
+                nums[L], nums[m] = nums[m], nums[L]
+                print(nums)
+                L+=1
+
+            elif nums[m]==2:
+                nums[m], nums[R] = nums[R], nums[m]
+                print(nums)
+                R-=1
+                m-=1
+            m+=1
