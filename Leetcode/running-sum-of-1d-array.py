11ae5c1ee672383591cdb58c39b14e8fbdab4ce4
Abdi Dawud <139214089+Abdidawud@users.noreply.github.com>
2024-01-24 17:38:44 +0300

    Added solution for Running Sum of 1d Array

---
+++ Leetcode/running-sum-of-1d-array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        ans=[]
+        running_sum=0
+        for i in nums:
+            running_sum+=i
+            ans.append(running_sum)
+        return ans
