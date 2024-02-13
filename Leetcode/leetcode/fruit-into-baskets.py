15e8d3bac1c1e74d92d1784375377b72a2abc7ad
Abdi Dawud <139214089+Abdidawud@users.noreply.github.com>
2024-02-13 22:46:35 +0300

    Add solution for Fruit Into Baskets

---
+++ Leetcode/leetcode/fruit-into-baskets.py
@@ -0,0 +1,17 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        ans = 0
+        i =0
+        j = 0
+        dic = defaultdict(int)
+
+        while j < len(fruits):
+            dic[fruits[j]]+=1
+            while len(dic) > 2:
+                dic[fruits[i]] -= 1
+                if dic[fruits[i]] == 0:
+                    dic.pop(fruits[i])
+                i+=1
+            j+=1
+            ans = max(ans, j-i)
+        return ans
