4f1790bb448dcadac2aca7a98861e15bf3a84193
Abdi Dawud <139214089+Abdidawud@users.noreply.github.com>
2024-05-21 17:49:29 +0300

    Created 'Heap Operations' by A2SV Hub

---
+++ 21-May-2024/Heap Operations 156452.py
@@ -0,0 +1,37 @@
+# Problem: Heap Operations - https://codeforces.com/contest/681/problem/C
+
+import heapq
+n=int(input())
+ans=[]
+heap=[]
+
+for _ in range(n):
+    op=input().split()
+    if op[0]=="insert":
+        temp=int(op[1])
+        heapq.heappush(heap,temp)
+        ans.append((op[0],temp))
+    
+    elif op[0]=="removeMin":
+        if not heap:
+            ans.append(("insert",1))
+        else:
+            heapq.heappop(heap)
+        ans.append((op[0],None))
+
+    else:
+        temp=int(op[1])
+        while heap and heap[0]<temp:
+            heapq.heappop(heap)
+            ans.append(("removeMin",None))
+        if not heap or heap[0]>temp:
+            heapq.heappush(heap,temp)
+            ans.append(("insert",temp))
+        ans.append((op[0],temp))
+# print(ans)
+print(len(ans))
+for i in ans:
+    if i[1] is not None:
+        print(i[0],i[1])
+    else:
+        print(i[0])
