42c87619ac802b207d90bd75466401a27a8a647d
Abdi Dawud <139214089+Abdidawud@users.noreply.github.com>
2024-06-01 12:12:02 +0300

    Created 'Maximum Depth of N-ary Tree' by A2SV Hub

---
+++ 01-Jun-2024/Maximum Depth of N-ary Tree 162953.py
@@ -0,0 +1,18 @@
+# Problem: Maximum Depth of N-ary Tree - https://leetcode.com/problems/maximum-depth-of-n-ary-tree/
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        if not root:
+            return 0
+        ans=0
+        for i in root.children:
+            ans=max(ans,self.maxDepth(i))
+        return 1+ans
