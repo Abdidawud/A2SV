9a4f5a6f2f205b920b10f6dd6353c0d94bf3bdfc
Abdi Dawud <139214089+Abdidawud@users.noreply.github.com>
2024-06-05 15:09:11 +0300

    Created 'D - Meron and Games' by A2SV Hub

---
+++ A2SV G5 - Contest #16 05-Jun-2024/D - Meron and Games 164366.py
@@ -0,0 +1,27 @@
+# Problem: D - Meron and Games - https://codeforces.com/gym/523525/problem/D
+
+import sys, threading
+from collections import *
+input = lambda: sys.stdin.readline().strip()
+
+def main():
+    store={}
+    n=int(input())
+    li=[int(i) for i in input().split()]
+    count=Counter(li)
+    max_=max(li)
+    def dp(num):
+        if num>max_:
+            return 0
+        if num not in store:
+            store[num]=max((count[num]*num)+dp(num+2),dp(num+1))
+        return store[num]
+    print(dp(1))
+if __name__ == '__main__':
+    
+    sys.setrecursionlimit(1 << 30)
+    threading.stack_size(1 << 27)
+
+    main_thread = threading.Thread(target=main)
+    main_thread.start()
+    main_thread.join()
