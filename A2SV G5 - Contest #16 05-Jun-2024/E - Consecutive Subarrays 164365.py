84934e4c81db0bb907662d7069da0369b9a1d10e
Abdi Dawud <139214089+Abdidawud@users.noreply.github.com>
2024-06-05 15:09:05 +0300

    Created 'E - Consecutive Subarrays' by A2SV Hub

---
+++ A2SV G5 - Contest #16 05-Jun-2024/E - Consecutive Subarrays 164365.py
@@ -0,0 +1,13 @@
+# Problem: E - Consecutive Subarrays - https://codeforces.com/gym/523525/problem/E
+
+n,k=(int(i) for i in input().split())
+li=[int(i) for i in input().split()]
+s_sum=[0]
+for i in range(n-1,-1,-1):
+    s_sum.append(s_sum[-1]+li[i])
+s_sum=s_sum[::-1]
+s_sum.pop()
+if k-1==0:
+    print(s_sum[0])
+else:
+    print(s_sum[0]+ sum(sorted(s_sum[1:])[-k+1:]))
