ecc4bbe813a55e05d2a30d931ec5e50ef7998bc8
Abdi Dawud <139214089+Abdidawud@users.noreply.github.com>
2024-06-05 15:10:37 +0300

    Created 'D -  Longest Good Sequence' by A2SV Hub

---
+++ A2SV G5 - Contest #17 05-Jun-2024/D -  Longest Good Sequence 164368.py
@@ -0,0 +1,29 @@
+# Problem: D -  Longest Good Sequence - https://codeforces.com/gym/524965/problem/D
+
+import sys
+n = int(sys.stdin.readline().strip())
+nums = list(map(int, sys.stdin.readline().strip().split()))
+if n == 1 and nums[0] == 1:
+    print(1)
+    sys.exit()
+
+def helper(num):
+    divisors = []
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            divisors.append(i)
+            if i != num // i:
+                divisors.append(num // i)
+
+    return divisors  
+
+dp = [0] *(max(nums) + 1)
+for num in nums:
+    divisors = helper(num)
+    dp[num] = 1
+    for div in divisors:
+        dp[num] = max(dp[num], dp[div] + 1)
+
+    for div in divisors:
+        dp[div] = max(dp[div], dp[num])
+print(max(dp))
