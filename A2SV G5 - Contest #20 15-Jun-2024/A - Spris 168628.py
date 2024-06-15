efb831caeaec52941c075da9df0c17fae19d9867
Abdi Dawud <139214089+Abdidawud@users.noreply.github.com>
2024-06-15 19:49:27 +0300

    Created 'A - Spris' by A2SV Hub

---
+++ A2SV G5 - Contest #20 15-Jun-2024/A - Spris 168628.py
@@ -0,0 +1,9 @@
+# Problem: A - Spris - https://codeforces.com/gym/528792/problem/A
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+temp=min(a // 1, b // 2, c // 4)
+ans=temp*7
+print(ans)
