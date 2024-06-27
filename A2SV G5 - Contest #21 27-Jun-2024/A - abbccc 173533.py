11374ab3ce474f326b949ce6ee3f4f512b6ec2d7
Abdi Dawud <139214089+Abdidawud@users.noreply.github.com>
2024-06-28 00:31:21 +0300

    Created 'A - abbccc' by A2SV Hub

---
+++ A2SV G5 - Contest #21 27-Jun-2024/A - abbccc 173533.py
@@ -0,0 +1,13 @@
+# Problem: A - abbccc - https://codeforces.com/gym/530187/problem/A
+
+n=int(input())
+s=input()
+
+i=0
+j=1
+ans=[]
+while i<len(s):
+    ans.append(s[i])
+    i+=j
+    j+=1
+print(''.join(ans))
