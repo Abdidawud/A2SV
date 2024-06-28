c6d13bd129ca872bb9cd65562b05977f565dcbd3
Abdi Dawud <139214089+Abdidawud@users.noreply.github.com>
2024-06-28 10:25:54 +0300

    Created 'C - Coffee Dilemma' by A2SV Hub

---
+++ A2SV G5 - Contest #20 28-Jun-2024/C - Coffee Dilemma 173575.py
@@ -0,0 +1,13 @@
+# Problem: C - Coffee Dilemma - https://codeforces.com/gym/528792/problem/C
+
+import heapq
+n = int(input())
+a = [int (i) for i in input().split()]
+li= []
+total_sum = 0
+for i in a:
+	total_sum += i
+	heapq.heappush(li, i)
+	while total_sum < 0:
+		total_sum -= heapq.heappop(li)
+print(len(li))
