98fa42c5128c78768d13ef4093253ff611f156b4
Abdi Dawud <139214089+Abdidawud@users.noreply.github.com>
2024-06-28 10:25:53 +0300

    Created 'B - BANless' by A2SV Hub

---
+++ A2SV G5 - Contest #20 28-Jun-2024/B - BANless 173574.py
@@ -0,0 +1,16 @@
+# Problem: B - BANless - https://codeforces.com/gym/528792/problem/B
+
+for _ in range(int(input())):
+    n = int(input())
+    li= []
+    
+    start, end = 1, 3 * n 
+    while start < end:
+        li.append([start, end])
+        start += 3
+        end -= 3    
+
+    print(len(li))
+    for i in li:
+        print(*i) 
+    
