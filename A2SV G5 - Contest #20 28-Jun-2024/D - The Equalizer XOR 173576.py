44099e04000d90f4549cb0fa17d8256737bc1af7
Abdi Dawud <139214089+Abdidawud@users.noreply.github.com>
2024-06-28 10:25:59 +0300

    Created 'D - The Equalizer XOR' by A2SV Hub

---
+++ A2SV G5 - Contest #20 28-Jun-2024/D - The Equalizer XOR 173576.py
@@ -0,0 +1,27 @@
+# Problem: D - The Equalizer XOR - https://codeforces.com/gym/528792/problem/D
+
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = [int(i) for i in input().split()]
+
+    pref_xor = [a[0]]
+    for i in range(1, n):
+        pref_xor.append(pref_xor[-1] ^ a[i])
+    yes = False
+    if pref_xor[-1] == 0:
+        yes = True
+    for i in range(n):
+        if yes:
+            break
+        first = pref_xor[i]
+        for j in range(i + 1, n):
+            second = pref_xor[j] ^ pref_xor[i]
+            third = pref_xor[-1] ^ pref_xor[j]
+            if first == second == third:
+                yes = True
+                break
+
+    print('YES' if yes else 'NO')
