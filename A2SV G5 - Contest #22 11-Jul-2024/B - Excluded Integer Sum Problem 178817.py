7212f6a32aadb178fd4211c33f7c3679fc8e9149
Abdi Dawud <139214089+Abdidawud@users.noreply.github.com>
2024-07-11 12:03:58 +0300

    Created 'B - Excluded Integer Sum Problem' by A2SV Hub

---
+++ A2SV G5 - Contest #22 11-Jul-2024/B - Excluded Integer Sum Problem 178817.py
@@ -0,0 +1,26 @@
+# Problem: B - Excluded Integer Sum Problem - https://codeforces.com/gym/531455/problem/B
+
+def solve():
+    n, k, x = map(int, input().split())
+
+    if x != 1:
+        print("YES")
+        print(n)
+        print("1 " * n)
+    else:
+        if k >= 2 and n % 2 == 0:
+            print("YES")
+            print(n // 2)
+            print("2 " * (n // 2))
+        elif k >= 3:
+            print("YES")
+            print(n // 2, "3", end=" ")
+            print("2 " * (n // 2 - 1))
+        else:
+            print("NO")
+
+if __name__ == "__main__":
+    T = 1
+    T = int(input())
+    for _ in range(T):
+        solve()
