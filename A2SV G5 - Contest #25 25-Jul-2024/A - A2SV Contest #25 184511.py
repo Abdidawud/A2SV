1131fb59772b621a9f2d24a6a388a66574c4a55d
Abdi Dawud <139214089+Abdidawud@users.noreply.github.com>
2024-07-25 17:25:12 +0300

    Created 'A - A2SV Contest #25' by A2SV Hub

---
+++ A2SV G5 - Contest #25 25-Jul-2024/A - A2SV Contest #25 184511.py
@@ -0,0 +1,16 @@
+# Problem: A - A2SV Contest #25 - https://codeforces.com/gym/535749/problem/A
+
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+a, b, c, d = map(int, input().split())
+def find_points(p, t):
+    return max((3*p)//10, p - (p*t)//250)
+m = find_points(a, c)
+v = find_points(b, d)
+
+if m > v:
+    print('Misha')
+elif m < v:
+    print('Vasya')
+else:
+    print('Tie')
