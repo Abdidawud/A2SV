28d6534557070a7d9a6801759c28139086c59f3c
Abdi Dawud <139214089+Abdidawud@users.noreply.github.com>
2024-07-25 17:25:18 +0300

    Created 'B - Optimal Point' by A2SV Hub

---
+++ A2SV G5 - Contest #25 25-Jul-2024/B - Optimal Point 184512.py
@@ -0,0 +1,17 @@
+# Problem: B - Optimal Point - https://codeforces.com/gym/535749/problem/B
+
+t = int(input())
+for _ in range(t):
+    N, K = map(int, input().split())
+    l_ = r_ = False
+    for _ in range(N):
+        l, r = map(int, input().split())   
+        if l == K:
+            l_ = True
+        if r == K:
+            r_ = True
+    
+    if l_ and r_:
+        print("YES")
+    else:
+        print("NO")
