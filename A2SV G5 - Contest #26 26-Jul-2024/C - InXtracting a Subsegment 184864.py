005845b1dbb9b794e02716e8bab1a0e20e3a6c9e
Abdi Dawud <139214089+Abdidawud@users.noreply.github.com>
2024-07-26 09:33:05 +0300

    Created 'C - InXtracting a Subsegment' by A2SV Hub

---
+++ A2SV G5 - Contest #26 26-Jul-2024/C - InXtracting a Subsegment 184864.py
@@ -0,0 +1,11 @@
+# Problem: C - InXtracting a Subsegment - https://codeforces.com/gym/537362/problem/C
+
+a,b=(int(i) for i in input().split())
+li=[int(i) for i in input().split()]
+
+if b==1:
+    print(min(li))
+elif b==2:
+    print(max(li[0],li[-1]))
+else:
+    print(max(li))
