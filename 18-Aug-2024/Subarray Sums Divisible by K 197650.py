54dc8c5a766820bee15afaa2ef3a2f1c76169c90
Abdi Dawud <139214089+Abdidawud@users.noreply.github.com>
2024-08-18 21:34:09 +0300

    Created 'Subarray Sums Divisible by K' by A2SV Hub

---
+++ 18-Aug-2024/Subarray Sums Divisible by K 197650.py
@@ -0,0 +1,17 @@
+# Problem: Subarray Sums Divisible by K - https://leetcode.com/problems/subarray-sums-divisible-by-k/
+
+class Solution:
+    def subarraysDivByK(self, nums: List[int], k: int) -> int:
+        dic=defaultdict(int)
+        dic[0]=1
+        ans=0
+        acc=0
+
+        for i in range(len(nums)):
+            acc+=nums[i]
+            temp=acc % k
+            if temp in dic:
+                ans += dic[temp]
+            dic[temp] +=1
+        
+        return ans
