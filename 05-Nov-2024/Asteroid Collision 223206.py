64ea282c943d1c558f0fe502c2cd587f315b41d7
Abdi Dawud <139214089+Abdidawud@users.noreply.github.com>
2024-11-05 20:00:22 +0300

    Created 'Asteroid Collision' by A2SV Hub

---
+++ 05-Nov-2024/Asteroid Collision 223206.py
@@ -0,0 +1,17 @@
+# Problem: Asteroid Collision - https://leetcode.com/problems/asteroid-collision/
+
+def asteroidCollision(asteroids):
+    stack = []
+    
+    for asteroid in asteroids:
+        while stack and asteroid < 0 < stack[-1]:
+            if stack[-1] < -asteroid:
+                stack.pop()
+                continue
+            elif stack[-1] == -asteroid:
+                stack.pop()
+            break
+        else:
+            stack.append(asteroid)
+    
+    return stack
